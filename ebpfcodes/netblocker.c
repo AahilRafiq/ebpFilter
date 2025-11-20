2238120890ede060a7b0accf54165fbe00a2f121
AahilRafiq <aahil.rafiq136@gmail.com>
2025-11-20 14:29:07 +0530

    slightly better code

--- ebpfcodes/netblocker.c
+++ ebpfcodes/netblocker.c
@@ -23,7 +23,7 @@ int sockfilter_netblocker_func(struct __sk_buff *skb) {
     void *data_end = (void *)(long)skb->data_end;
     void *data = (void *)(long)skb->data;
     __u64 offset = 0;
-
+    
     offset = parse_eth(data, data_end);
     if(offset == -1) {
         bpf_printk("returning here");
@@ -42,36 +42,30 @@ int sockfilter_netblocker_func(struct __sk_buff *skb) {
     __u16 num_queries = ntohs(dnsh->qdcount);
     offset += sizeof(struct dns_hdr);
 
+    char buffer[256];
     while(true) {
-        void *ptr = data + offset;
-        if(ptr + 1 > data_end) {
-            break;
+        __u8 label_len;
+        if(bpf_skb_load_bytes(skb, offset, &label_len, 1) < 0) {
+            return TCX_NEXT;
         }
-        __u8 wordlen = *(__u8*)ptr;
-        ptr += 1;
-
-        if(ptr + wordlen > data_end) {
+        offset += 1;
+        
+        if(label_len == 0) {
             break;
         }
         
-        const char* byte_ptr = (const char*)ptr;
-        if((void*)byte_ptr + wordlen > data_end) {
+        if(bpf_skb_load_bytes(skb, offset, (void*)buffer, label_len) < 0) {
             return TCX_NEXT;
         }
 
-        char word[256];
-        for(__u8 i=0; i<wordlen; i++) {
-            if ((void *)byte_ptr + i + 1 > data_end)
-                return TCX_NEXT;
-            word[i] = byte_ptr[i];
-        }
-        word[wordlen] = '\0';
-        bpf_printk("%s",word);
-        if(strcmp(word, "neetcode") == 0) {
-            bpf_printk("Dropping LEETCODE");
-            return 2;
+        buffer[label_len] = '\0';
+        bpf_printk("%s",buffer);
+        if(strcmp(buffer, "leetcode") == 0) {
+            bpf_printk("DROPPING");
+            return TCX_DROP;
         }
 
+        offset += label_len;
         break;
     }
 
